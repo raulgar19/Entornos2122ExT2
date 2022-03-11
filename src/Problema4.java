85a81af246c3c134117561c82278a857bd58af04
Raul García <raulgamu2003@gmail.com>
2022-03-11 11:40:36 +0100

    Resolución del problema 4

---
+++ src/Problema4.java
@@ -0,0 +1,49 @@
+public class Problema4 {
+    private static boolean esNumeroPrimo(Integer num){
+        boolean encontrado = true;
+        if (num<=1) {
+            encontrado= false;
+        }
+        if (encontrado){
+            for (int i = 2; i <= num / 2; ++i) {
+                if (num % i == 0) {
+                    encontrado= false;
+                    break;
+                }
+            }
+        }
+        return encontrado;
+    }
+    static String siPrimo(int num){
+        String res = "";
+        for (int i = 1; i <= num; i++){
+            if (num % 3 == 0){
+                res += num + "Fizz";
+                if (esNumeroPrimo(num) == true){
+                    res += num + "FizzWhiz";
+                }
+            }
+            if (num % 5 == 0){
+                res += num + "Buzz";
+                if (esNumeroPrimo(num) == true){
+                    res += num + "BuzzWhiz";
+                }
+            }
+            if (num % 3 == 0 && num % 5 == 0){
+                res += num + "FizzBuzz";
+                if (esNumeroPrimo(num) == true){
+                    res += num + "FizzBuzzWhiz";
+                }
+            }
+            else{
+
+            }
+        }
+        return res;
+    }
+    static String programa(int num) {
+        String res = "";
+        res = siPrimo(num);
+        return res;
+    }
+}
---
+++ test/TestProblema4.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class TestProblema4 {
+    @Test
+    public void devuelve_cadena_de_multipos(){
+        //Arrange
+        Problema3 problema3 = new Problema3();
+
+        //Act
+        problema3.programa(5);
+
+        //Asserts
+        Assert.equals("1FizzBuzzWhizz3FizzWhizz5BuzzWhizz", devuelve_cadena_de_multipos());
+    }
+}
